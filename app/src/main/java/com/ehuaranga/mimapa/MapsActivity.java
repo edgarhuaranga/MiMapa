d5ed0a9ea188e1ff3c72e237d4fe964f4f31be3b
Edgar Huaranga <edhu1227@gmail.com>
2018-03-14 19:54:41 -0500

    decode and onResponse

--- app/src/main/java/com/ehuaranga/mimapa/MapsActivity.java
+++ app/src/main/java/com/ehuaranga/mimapa/MapsActivity.java
@@ -1,12 +1,16 @@
 package com.ehuaranga.mimapa;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,8 +21,15 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,Response.Listener<String>, Response.ErrorListener {
 
     private GoogleMap mMap;
 
@@ -59,7 +70,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 pucp.latitude+","+pucp.longitude+"&key=AIzaSyC9o0DddWB5zziv1-kcqqJG-rvDGI3BxVg";
 
 
-        StringRequest request = new StringRequest(Request.Method.GET, rutaUniPucp, this);
+        StringRequest request = new StringRequest(Request.Method.GET, rutaUniPucp, this, this);
         requestQueue.add(request);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+    }
+
+    @Override
+    public void onResponse(String response) {
+        try {
+            // Tranform the string into a json object
+            final JSONObject json = new JSONObject(response);
+            JSONArray routeArray = json.getJSONArray("routes");
+            JSONObject routes = routeArray.getJSONObject(0);
+            JSONObject overviewPolylines = routes.getJSONObject("overview_polyline");
+            String encodedString = overviewPolylines.getString("points");
+            JSONArray legs = routes.getJSONArray("legs");
+            int totalDistance = 0;
+            int partialDistance = 0;
+            for(int i=0; i<legs.length(); i++){
+                JSONObject jsonLeg = legs.getJSONObject(i);
+                JSONObject jsonDuration = jsonLeg.getJSONObject("duration");
+                partialDistance = jsonDuration.getInt("value");
+                totalDistance += partialDistance;
+            }
+
+            List<LatLng> list = decodePoly(encodedString);
+
+            PolylineOptions options = new PolylineOptions().width(15).color(Color.RED).geodesic(true);
+            for (int z = 0; z < list.size(); z++) {
+                LatLng point = list.get(z);
+                options.add(point);
+            }
+            if(ultimatePolyline != null) ultimatePolyline.remove();
+            ultimatePolyline = mMap.addPolyline(options);
+            Log.d("tiempo", totalDistance+"");
+            //textViewJourneyDuration.setText("Time to Allianz Arena: "+totalDistance/60+"min");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List decodePoly(String encoded) {
+
+        List poly = new ArrayList();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
     }
 }
